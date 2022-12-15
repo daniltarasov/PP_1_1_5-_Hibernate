e5b2d684832267ca273176db72c4275976ba6e4a
ADMIN <daniltarasov@yandex.ru>
2022-12-15 17:26:57 +0300

    fixing remarks

--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -9,13 +9,10 @@ public class User {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column (name = "name")
     private String name;
 
-    @Column (name = "lastName")
     private String lastName;
 
-    @Column (name = "age")
     private Byte age;
 
     public User() {
